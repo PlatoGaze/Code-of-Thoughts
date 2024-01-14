3d971a05fd391e95bf77aa940b0aa143ce6d4e1f
Wilson <wilsonyang315@gmail.com>
2024-01-14 13:53:08 +0800

    add run

---
+++ python/tests/test_run.py
@@ -0,0 +1,14 @@
+import json
+import sys
+import os
+sys.path.append(os.getcwd() + r"/src")
+import platogaze
+
+file_path = "tests/code.json"
+
+# Load JSON data from file
+with open(file_path, 'r') as file:
+    script = json.load(file)
+
+# Traverse the function starting from the root
+platogaze.run(script["template"])  # template["template"] is a list of codes or expressions
