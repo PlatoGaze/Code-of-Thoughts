9043b7a79f23274a3cb00e5118c4105f1bd7c0e1
Wilson <wilsonyang315@gmail.com>
2024-01-15 12:02:33 +0800

    add doc strings

--- python/template.py
+++ python/template.py
@@ -3,7 +3,16 @@ import json
 
 class Template:
     def __init__(self, template_dict: dict) -> None:
-        if (self.is_valid(template_dict)):
+        """
+        Initializes an instance of MyClass with a template dictionary.
+
+        Args:
+            template_dict (dict): A dictionary representing the template.
+
+        Raises:
+            ValueError: If the template format is invalid.
+        """
+        if self.is_valid(template_dict):
             self.template = template_dict
         else:
             raise ValueError("Invalid template format")
@@ -12,12 +21,27 @@ class Template:
         return True
 
     def to_dict(self):
+        """
+        Converts the template object to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the template object.
+        """
         return self.template.copy()
 
     def display(self):
         print(json.dumps(self.to_dict(), indent=4))
 
     def get_template_by_id(self, id: str) -> 'Template':
+        """
+        Retrieves a template by its ID.
+
+        Args:
+            id (str): The ID of the template.
+
+        Returns:
+            Template: The template object corresponding to the given ID, or None if not found.
+        """
         references = id.split('.')
         template = self.template.copy()
         path_so_far = []
@@ -39,6 +63,16 @@ class Template:
         return Template(template)
 
     def add_template(self, new_template, id: str = ""):
+        """
+        Adds a new template to the existing template.
+
+        Parameters:
+        - new_template: The new template to be added.
+        - id: The optional ID specifying the location where the new template should be added. If not provided, the new template will be added at the end.
+
+        Returns:
+        None
+        """
         references = id.split('.')
         template = self.template.copy()
         if id != "":
